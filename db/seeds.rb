d7f5c3fb6abdb6d6a2ec4a2d5db5dd11ee71ccb6
muraapp <bergsonisme@gmail.com>
2016-11-25 19:15:59 +0900

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,16 +2,22 @@
     name  = Faker::Name.name
     email = Faker::Internet.email
     content = Faker::Lorem.sentence
-    user_id = n + 1
-    topic_id = n + 1
+
+
+    uid = User.create_unique_string
+    provider = "facebook"
     password = "password"
     User.create!(name: name,
                 email: email,
+                uid: uid,
+                provider: provider,
                 password: password,
                 password_confirmation: password)
+    user_id = User.last.id
 
     Topic.create!(user_id: user_id,
                  content: content)
+    topic_id = Topic.last.id
 
     Comment.create!(topic_id: topic_id,
                   user_id: user_id,
