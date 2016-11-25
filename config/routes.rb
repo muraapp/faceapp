3479d725226ca371a8bd90b26d443acb38f0deb7
muraapp <bergsonisme@gmail.com>
2016-11-25 18:54:09 +0900

    fix sign-up

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
 
   devise_for :users, controllers: {
-    omniauth_callbacks: "users/omniauth_callbacks"
+    omniauth_callbacks: "users/omniauth_callbacks",
+    registrations: "users/registrations"
   }
 
   resources :topics do
