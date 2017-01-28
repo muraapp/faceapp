9ea2f1111b171378be2bcde7f50e530b574f8842
muraapp <bergsonisme@gmail.com>
2017-01-28 15:58:55 +0900

    fix carrierwave configassethost

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -13,7 +13,7 @@ CarrierWave.configure do |config|
   case Rails.env
     when 'production'
       config.fog_directory = 'faceapp-pro'
-      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/faceapp-production'
+      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/faceapp-pro'
     # when 'development'
     #   config.fog_directory = 'faceapp-development'
     #   config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/faceapp-development'
