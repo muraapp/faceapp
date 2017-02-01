642c835bc0f7756492b6c1e3777b2a1e85f89eec
muraapp <bergsonisme@gmail.com>
2017-02-01 20:45:16 +0900

    0201 aws set

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -12,10 +12,10 @@ CarrierWave.configure do |config|
 
   case Rails.env
     when 'production'
-      config.fog_directory = 'faceapp-kazu-production'
-      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/faceapp-kazu-production'
+      config.fog_directory = 'faceapp-production'
+      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/faceapp-production'
     when 'development'
       config.fog_directory = 'faceapp-development'
-      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/faceapp-kazu-development'
+      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/faceapp-development'
   end
 end
